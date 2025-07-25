592aa6929c5f6303d01023924769661bcca9d103
joonho16 <rhwnsgh0724@naver.com>
2025-07-25 12:39:34 +0900

    constant br_hand에 맞게 파라미터 수정

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 DATA_DIR = './datasets'
 TASK_CONFIGS = {
     'grasp_ball':{
-        'dataset_dir': DATA_DIR + '/grasp_cable_yaskawa',
-        'num_episodes': 36,
-        'episode_len': 120,
+        'dataset_dir': DATA_DIR + '/grasp_ball',
+        'num_episodes': 1,
+        'episode_len': 50,
         'camera_names': ['camera/camera'],
         'camera_config': {
             'camera/camera': {
@@ -19,6 +19,8 @@ TASK_CONFIGS = {
     }
 }
 
+DEFAULT_CAMERA_NAMES = ['camera/camera']
+
 TOPIC_NAME = {
     'br_hand': {
         # 'joint_state': 'joint_state',
@@ -28,7 +30,7 @@ TOPIC_NAME = {
 }
 
 JOINT_NAMES = {
-    # 'br_hand': ['joint1', 'joint2', 'joint3', 'joint4']
+    'br_hand': ['thumb/FE', 'thumb/ABD', 'index/FE', 'middle/FE', 'ring/FE']
 }
 
 TOOL_NAMES = {
@@ -37,28 +39,28 @@ TOOL_NAMES = {
 }
 
 JOINT_LIMIT = {
-    # 'br_hand': {
-    #     'joint1': {
-    #         'min': -3.142,
-    #         'max': 3.142,
-    #     },
-    #     'joint2': {
-    #         'min': -2.050,
-    #         'max': 1.571,
-    #     },
-    #     'joint3': {
-    #         'min': -1.571,
-    #         'max': 1.530,
-    #     },
-    #     'joint4': {
-    #         'min': -1.800,
-    #         'max': 2.000,
-    #     },
-    #     'gripper': {
-    #         'min': -0.01,
-    #         'max': 0.01,
-    #     },
-    # },
+    'br_hand': {
+        'thumb/FE': {
+            'min': -4.35,
+            'max': 0.41,
+        },
+        'thumb/ABD': {
+            'min': 3.85,
+            'max': 1.8,
+        },
+        'index/FE': {
+            'min': -0.25,
+            'max': 6.27,
+        },
+        'middle/FE': {
+            'min': -4.83,
+            'max': 1.9,
+        },
+        'ring/FE': {
+            'min': -5.56,
+            'max': 0.9,
+        },
+    },
 }
 
 DT = 0.1
